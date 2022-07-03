5432c6c528d121d7bcb70992a9999215c57cfe44
Dakkaoui050 <80848743+Dakkaoui050@users.noreply.github.com>
2022-07-03 10:51:59 +0200

    healthbar for thee player

--- shootingGame/Assets/script/Game/PlayerHealth.cs
+++ shootingGame/Assets/script/Game/PlayerHealth.cs
@@ -3,44 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class health : MonoBehaviour
-{   //health
-    public float Health;  
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
     public float MaxHealth;
-
-    //range of damage. (I chance this later voor each weapon)
-    public int minDmg = 1;
-    public int maxDmg = 10;
-
 
     public GameObject HealthBar;
     public Slider Slider;
 
-    public void Damage(int amount)
-    {
-        Health -= Random.Range(minDmg, maxDmg);
-
-        Health -= amount;
-        if(Health <= 0)
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
-
     public void Start()
     {
-        Health = MaxHealth;
+        health = MaxHealth;
         Slider.value = CalculateHealth();
         //scoreText.text = "Points: " + 1;
     }
 
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+
+    }
 
     public void Update()
     {
         Slider.value = CalculateHealth();
 
-        if (Health <= 0)
+        if (health <= 0)
         {
 
             Destroy(gameObject);
@@ -50,14 +38,23 @@ public class health : MonoBehaviour
 
     float CalculateHealth()
     {//als de max health 100 is en health 10 veranderd de healthbar
-        return Health / MaxHealth;
+        return health / MaxHealth;
     }
 
 
-    
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+
+            Debug.Log("Enemy started colliding with player.");
 
 
+            this.health -= 10;
 
+        }
 
+    }
 
 }
